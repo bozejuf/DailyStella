f2c1f546da0ad704f09ffe2fe28f8026232a7c43
Brechtje <brechtje.git@bram.blmgroep.nl>
2024-06-26 12:13:44 +0200

    Eerste versie

---
+++ app.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def change_numbers():
+    file = open("DailyStella\pic_number.txt", "r")
+    pic_number = int(file.read())
+    if date_is_different():
+        pic_number += 1
+        file.close()
+        file = open("DailyStella\pic_number.txt", "w")
+        file.write(str(pic_number))
+        file.close()
+        file = open("DailyStella/last_update.txt", "w")
+        file.write(datetime.now().strftime("%d-%m-%Y"))
+        file.close()
+
+@app.route("/")
+def index():
+    change_numbers()
+    file = open("DailyStella\pic_number.txt", "r")
+    pic_number = f"Stella{file.read()}"
+    return render_template("index.html", pic_number=pic_number)
+
+# checks if the date of today is different than the date in last_update.txt
+def date_is_different():
+    # read late update-date in last_update (dd-mm-yyy format)
+    file = open("DailyStella\last_update.txt", "r")
+    last_update = file.read()
+
+    # save current date in current_date (dd-mm-yyyy format)
+    current_date = datetime.now().strftime("%d-%m-%Y")
+
+    if current_date == last_update:
+        return False
+    else:
+        return True
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
